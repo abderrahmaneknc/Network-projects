6183f3e45c1bc83c3db59926403d378db7273eaa
abderrahmaneknc <148714671+abderrahmaneknc@users.noreply.github.com>
2025-04-26 13:11:01 +0100

    Update devices.js

--- devices.js
+++ devices.js
@@ -1,12 +1,12 @@
 module.exports = {
   routers: [
-    { name: 'R1', port: 5012 },
-    { name: 'R2', port: 5013 },
-    { name: 'R3', port: 5014 }
+    { name: 'R1', port: 5025 },
+    { name: 'R2', port: 5026 },
+    { name: 'R3', port: 5027 }
   ],
   pcs: [
-    { name: 'PC1', port: 5018 },
-    { name: 'PC2', port: 5020 },
-    { name: 'PC3', port: 5022 }
+    { name: 'PC1', port: 5028 },
+    { name: 'PC2', port: 5030 },
+    { name: 'PC3', port: 5032 }
   ]
 };
